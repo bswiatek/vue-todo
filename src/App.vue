c76f8fd19430dd0c01a8064e7f90f83c911c4f04
Bartłomiej Świątek <emini1@swiatek.biz>
2020-02-06 19:54:23 +0100

    todo list layout

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h4 class="bg-primary text-white text-center p-2">Lista zadań użytkownika {{name}}</h4>
+    <div class="container-fluid p-4">
+      <div class="row">
+        <div class="col font-weight-bold">Zadanie</div>
+        <div class="col-2 font-weight-bold">Zakończono?</div>
+      </div>
+      <div class="row" v-for="t in tasks" v-bind:key="t.action">
+        <div class="col">{{t.action}}</div>
+        <div class="col-2">{{t.done}}</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      name: "Bartek",
+      tasks: [
+        { action: "Kup kwiaty", done: false},
+        { action: "Znajdź buty", done: false},
+        { action: "Odbierz bilety", done: true},
+        { action: "Zadzwoń do Janka", done: false},
+      ]
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
