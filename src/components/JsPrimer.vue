c87fb72606fba5b38f135f9fbe67943484e543e6
Bartłomiej Świątek <emini1@swiatek.biz>
2020-02-16 11:37:37 +0100

    jsPrimer object data

--- src/components/JsPrimer.vue
+++ src/components/JsPrimer.vue
@@ -11,4 +11,14 @@ export default {
 const myFunc = (nameFunction) => ("Cześć, " + nameFunction() + "!");
 const printName = (nameFunction, printFunction) => printFunction(myFunc(nameFunction));
 printName(function() {return "Bartek"}, console.log)
+
+let myData = {
+  name : "Bartek",
+  weather : "słonecznie",
+  printMessages() {
+    console.log(`Hi, ${this.name}!`);
+    console.log(`Dziś jest ${this.weather}!`);
+  }
+};
+myData.printMessages();
 </script>
